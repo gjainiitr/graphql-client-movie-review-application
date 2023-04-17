4ad8efc18d294bfdb75ff2cef0ea0854f569e58e
gjainiitr <47331030+gjainiitr@users.noreply.github.com>
2023-04-17 18:29:34 +0530

    Writing demo query in Apollo and verifying it

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
+client.query({
+  query: gql`
+    query GetMovies {
+      getMovies {
+        title
+      }
+    }
+  `
+})
+.then((result) => console.log(result));
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
