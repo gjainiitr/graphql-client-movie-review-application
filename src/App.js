1d8e82441b22b6c987ce1022d6742bc486fc312d
gjainiitr <47331030+gjainiitr@users.noreply.github.com>
2023-04-17 18:41:14 +0530

    Basic example to use Apollo client

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_MOVIES = gql`
+  query GetMovies {
+    getMovies {
+      id
+      title
+      year
+      rating
+      genres
+    }
+  }
+`;
 
 function App() {
+
+  const {loading, error, data} = useQuery(GET_MOVIES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
     <div className="App">
-      
+      {
+        data.getMovies.map((movie) => {
+          return (
+            <div>{movie.title}</div>
+          )
+        })
+      }
     </div>
   );
 }
